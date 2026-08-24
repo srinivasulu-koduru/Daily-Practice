fd1b35b079a0dd07dbfe0e37b36c7e23e66979c8
Srinivasulu Koduru <srinivasulu3621@gmail.com>
2026-08-24 22:31:58 +0530

    ✅ GCD of two numbers | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ gfg/basic/gcd-of-two-numbers3459/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int gcd(int a, int b) {
+        // code here
+       while(a>0&&b>0)
+       {
+           if(a>b)
+               a=a%b;
+           else
+               b=b%a;
+       }
+       if(a==0)
+           return b;
+       else
+           return a;
+    }
+}
