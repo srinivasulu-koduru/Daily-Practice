b188080c8f9aa7eb28c59fe638de96dd310ac83b
Srinivasulu Koduru <srinivasulu3621@gmail.com>
2026-08-25 22:22:42 +0530

    ✅ Median of an Array | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ gfg/basic/find-the-median0527/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double findMedian(int[] arr) {
+        // Code here.
+        Arrays.sort(arr);
+        int n=arr.length;
+        if(n%2!=0)
+        {
+            return arr[n/2];
+        }
+        else
+        {
+            return (double)(arr[n/2-1]+arr[n/2])/2;
+        }
+        
+    }
+}
