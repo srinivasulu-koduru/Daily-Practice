fd31ef4ae94bbf793acc859aa6f1c45293e1d0b2
Srinivasulu Koduru <srinivasulu3621@gmail.com>
2026-08-26 20:30:46 +0530

    ✅ REGCLN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ codechef/medium/REGCLN/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(Math.abs(n%10-10));
+	}
+}
