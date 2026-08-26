63421007c72a8b29a709afdca4c491d30cb34082
Srinivasulu Koduru <srinivasulu3621@gmail.com>
2026-08-26 20:59:14 +0530

    ✅ POSHOP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/POSHOP/solution.java
+++ codechef/medium/POSHOP/solution.java
@@ -20,6 +20,8 @@ class Codechef
 		    int max=-1;
 		    for(int i=0;i<n;i++)
 		    {
+		        if(arr[i]>max)
+		            max=arr[i];
 		        for(int j=i+1;j<n;j++)
 		        {
 		            if(arr[i]<arr[j]&&arr[i]+arr[j]>max)
