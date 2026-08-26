c23982acbf5e574caea9851cdd1596f0c3062896
Srinivasulu Koduru <srinivasulu3621@gmail.com>
2026-08-26 21:21:06 +0530

    ✅ GRDJUMP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/GRDJUMP/solution.java
+++ codechef/medium/GRDJUMP/solution.java
@@ -7,7 +7,34 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		
+	  Scanner sc=new Scanner(System.in);
+	  int t=sc.nextInt();
+	  while(t-->0)
+	  {
+	      int a=sc.nextInt();
+	      int b=sc.nextInt();
+	      int p=sc.nextInt();
+	      int q=sc.nextInt();
+	      int r=sc.nextInt();
+	      int cost=0;
+	      if(a==b)
+	      {
+	          cost=r*2;
+	          System.out.println(cost);
+	      }
+	      while(a>0)
+	      {
+	          cost+=p;
+	          a/=2;
+	      }
+	      while(b>0)
+	      {
+	          cost+=q;
+	          b/=2;
+	      }
+	      System.out.println(cost);
+	      
+	  }
 
 	}
 }
