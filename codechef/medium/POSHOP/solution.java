4daef69d8c2fb66b1c158f6b6106b1475c788742
Srinivasulu Koduru <srinivasulu3621@gmail.com>
2026-08-26 20:32:40 +0530

    ✅ POSHOP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ codechef/medium/POSHOP/solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+
+	}
+}
