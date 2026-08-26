422f46b9acc1bdf3444bbc626547d6f5fdca9a0a
Srinivasulu Koduru <srinivasulu3621@gmail.com>
2026-08-26 20:49:14 +0530

    ✅ POSHOP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/POSHOP/solution.java
+++ codechef/medium/POSHOP/solution.java
@@ -7,7 +7,29 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int arr[]=new int[n];
+		    for(int i=0;i<n;i++)
+		    {
+		        arr[i]=sc.nextInt();
+		    }
+		    int max=arr[0];
+		    for(int i=0;i<n;i++)
+		    {
+		        for(int j=i+1;j<n;j++)
+		        {
+		            if(arr[i]<arr[j]&&arr[i]+arr[j]>max)
+		            {
+		                max=arr[i]+arr[j];
+		            }
+		        }
+		    }
+		    System.out.println(max);
+		}
 
 	}
 }
